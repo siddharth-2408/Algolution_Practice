4f9efe2b335ac6a6fbff997ef23555140c465c32
siddharth-2408 <gsiddharth2201@gmail.com>
2024-11-29 14:55:35 +0530

    Update Ps_1.java

--- src/set1/Ps_1.java
+++ src/set1/Ps_1.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package set1;
-
-/**
- *
- * @author rajes
- */
-public class Ps_1 {
+#include <stdio.h>
+void main()
+{
+    int i,j;
     
+    for(i=5;i>0;i--)
+    {
+        for(j=0;j<i;j++)
+        {
+            printf(" ");
+        }
+        for(int k=0;k<5;k++)
+        {
+            printf("*");
+        }
+        
+         printf("\n");
+    }
 }
