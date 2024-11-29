f3259d18f5cdacbe8a5b6c3c12800c4c7b4dbec9
siddharth-2408 <gsiddharth2201@gmail.com>
2024-11-29 14:58:19 +0530

    Create Ps_13.c

---
+++ src/set1/Ps_13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf(" ");
+        }
+        
+        for(j=1;j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=4;i>=1;i--)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf(" ");
+        }
+        
+        for(j=1;j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
